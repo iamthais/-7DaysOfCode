24fb3d187fa0bb54d62ee2f081010a809b2dc6e7
iamthais <stanchackthais@gmail.com>
2025-02-25 21:09:39 -0300

    Day1 #7DaysOfCode

---
+++ day1.js
@@ -0,0 +1,47 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+//Em JavaScript, a condição == é um operador de comparação que verifica se dois valores são iguais. 
+//No entanto pode converter os tipos dos valores antes de compará-los.
+
+if (numeroUm == stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (numeroTrinta == stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
+
+//O operador === (igualdade estrita) compara dois valores, verificando se eles são iguais e se têm o mesmo tipo.
+
+if (numeroUm === stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+  }
+  
+  if (numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+  } else {
+    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+  }
+  
+  if (numeroDez === stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  }
