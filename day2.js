4b41bd68f4dd25a76f92110fde24990571c25f72
Thais <stanchackthais@gmail.com>
2025-02-27 08:21:42 -0300

    Day2 #7DaysOfCode

--- day2.js
+++ day2.js
@@ -2,14 +2,14 @@ const nome = prompt("Qual o seu nome?");
 const idade = prompt("Quantos anos você tem?");
 const linguagem = prompt("Qual linguagem de programação você está estudando?");
 
-alert("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!")
+alert(`Olá ${nome} você tem ${idade} anos e já está aprendendo ${linguagem}!`)
 
-const resposta = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.")
+const resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`)
 
-if(resposta == 1){
+if (resposta == 1) {
     alert("Muito bom! Continue estudando e você terá muito sucesso.");
-}else if(resposta == 2){
+} else if (resposta == 2) {
     alert("Ahh que pena... Já tentou aprender outras linguagens?");
-} else{
+} else {
     alert("Número inválido.")
 }
